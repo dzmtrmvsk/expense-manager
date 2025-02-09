b8d08d7537075a661b7089a5656d1c5de82997b0
dzmtrmnvsk <dimadi2468@gmail.com>
2025-02-09 21:05:46 +0300

    feat: init commit

---
+++ src/test/java/com/expense/manager/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.expense.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Will be implemented
+	}
+
+}
